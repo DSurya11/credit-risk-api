be316451a176643e1fff92a172bf90e8f83f0b16
DSurya11 <suryadantuluri2005@gmail.com>
2026-01-07 10:13:05 +0530

    new changes in dockerfile

--- app/db/database.py
+++ app/db/database.py
@@ -2,23 +2,33 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 import os
 
-mysql_user = os.environ["MYSQLUSER"]
-mysql_password = os.environ["MYSQLPASSWORD"]
-mysql_host = os.environ["MYSQLHOST"]
-mysql_port = os.environ["MYSQLPORT"]
-mysql_db = os.environ["MYSQLDATABASE"]
+engine = None
+sessionlocal = None
 
-db_url = (
-    f"mysql+pymysql://{mysql_user}:{mysql_password}"
-    f"@{mysql_host}:{mysql_port}/{mysql_db}"
-)
-
-engine = create_engine(db_url, pool_pre_ping=True)
-
-sessionlocal = sessionmaker(bind=engine, autocommit=False, autoflush=False)
 base = declarative_base()
 
+def init_db():
+    global engine, sessionlocal
+
+    if engine is None:
+        mysql_user = os.environ["MYSQLUSER"]
+        mysql_password = os.environ["MYSQLPASSWORD"]
+        mysql_host = os.environ["MYSQLHOST"]
+        mysql_port = os.environ["MYSQLPORT"]
+        mysql_db = os.environ["MYSQLDATABASE"]
+
+        db_url = (
+            f"mysql+pymysql://{mysql_user}:{mysql_password}"
+            f"@{mysql_host}:{mysql_port}/{mysql_db}"
+        )
+
+        engine = create_engine(db_url, pool_pre_ping=True)
+        sessionlocal = sessionmaker(bind=engine, autocommit=False, autoflush=False)
+
 def get_db():
+    if sessionlocal is None:
+        init_db()
+
     db = sessionlocal()
     try:
         yield db
