92e2528a6dcc848fc4d6aea29cdadf2a9f5aaa90
DSurya11 <suryadantuluri2005@gmail.com>
2026-03-10 23:54:51 +0530

    fix prediction crash and mysql reconnect

--- app/db/database.py
+++ app/db/database.py
@@ -20,12 +20,8 @@ db_url = (
 engine = create_engine(
     db_url,
     pool_pre_ping=True,
-    pool_recycle=1800,
-    pool_size=5,
-    max_overflow=10,
-    connect_args={"connect_timeout": 10}
+    pool_recycle=280
 )
-
 sessionlocal = sessionmaker(
     bind=engine,
     autocommit=False,
